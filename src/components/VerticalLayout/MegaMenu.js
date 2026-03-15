7ba8023cd934ee1d28b8acdaaed1fbb17c6a10fc
sricharitha1998 <sricharitha1998@gmail.com>
2026-03-15 17:10:57 +0530

    removed megamenu

--- src/components/VerticalLayout/MegaMenu.js
+++ src/components/VerticalLayout/MegaMenu.js
@@ -17,7 +17,7 @@ class MegaMenu extends Component {
     render() {
         return (
             <React.Fragment>
-                <Dropdown className="dropdown-mega d-none d-lg-block ms-2" isOpen={this.state.megaMenu} toggle={() => { this.setState({ megaMenu: !this.state.megaMenu }) }}>
+                {/* <Dropdown className="dropdown-mega d-none d-lg-block ms-2" isOpen={this.state.megaMenu} toggle={() => { this.setState({ megaMenu: !this.state.megaMenu }) }}>
                     <DropdownToggle tag="button" type="button" caret className="btn header-item waves-effect">
                         {this.props.t('Mega Menu')}{" "}
                         <i className="mdi mdi-chevron-down"></i>
@@ -140,7 +140,7 @@ class MegaMenu extends Component {
                         </Row>
 
                     </DropdownMenu>
-                </Dropdown>
+                </Dropdown> */}
             </React.Fragment>
         );
     }
